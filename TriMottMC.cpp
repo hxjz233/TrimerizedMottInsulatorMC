dfabd4c014d7c54fbd132d3d03d39186e88ec128
Zhuowei Zhang <zzw_question@126.com>
2022-10-16 18:08:31 +0800

    First version, Heisenberg, 2*2

--- TriMottMC.cpp
+++ TriMottMC.cpp
@@ -28,24 +28,27 @@ class Lattice
             return configE;
         }
 
-        double GetMagnetization()
-        {
-            configM = 0;
-            for (int i=0; i<pow(m_size,2); i++)
-            {
-                configM += spinConfig[i];
-            }
-            return configM;
-        }
+        // double GetMagnetization()
+        // {
+        //     configM = 0;
+        //     for (int i=0; i<pow(m_size,2); i++)
+        //     {
+        //         configM += spinConfig[i];
+        //     }
+        //     return configM;
+        // }
 
         void DebugOutput()
         {
-            cout << spinConfig[0] << endl << configE;
+            ShowConfig();
+            cout << GetSiteInnerProduct(0,1) << endl;
         }
 
         ~Lattice()
         {
             delete temp_stackedIndex;
+            delete temp_randomSphericalDirection;
+            delete temp_selectedSiteSpin;
         }
 
     private:
@@ -58,9 +61,12 @@ class Lattice
         double deltaE;
         double configM;
 
-        int spinConfig[LATTICESIZE_MAX * LATTICESIZE_MAX];
+        double spinConfig[LATTICESIZE_MAX * LATTICESIZE_MAX][2];
         int neighbor[LATTICESIZE_MAX * LATTICESIZE_MAX][NEIGHBOR_MAX];
         int *temp_stackedIndex = new int[2];
+        double *temp_randomSphericalDirection = new double[2];
+        int temp_flipPos;
+        double *temp_selectedSiteSpin = new double[2];
 
         int GetFlattenedCoordinate(int stackedIndex1, int stackedIndex2)
         {
@@ -77,6 +83,18 @@ class Lattice
             temp_stackedIndex[0] = int(flattenedIndex / m_size);
             temp_stackedIndex[1] = flattenedIndex % m_size;
         }
+
+        void SetRandomSphericalDirection()
+        {
+            temp_randomSphericalDirection[0] = acos(1-2*realRand(engineTime));
+            temp_randomSphericalDirection[1] = 2 * M_PI * realRand(engineTime);
+        }
+
+        double GetSiteInnerProduct(int site1, int site2)
+        {
+            return sin(spinConfig[site1][0]) * sin(spinConfig[site2][0]) * cos(spinConfig[site1][1]-spinConfig[site2][1])
+                 + cos(spinConfig[site1][0]) * cos(spinConfig[site2][0]);
+        }
 };
 
 Lattice::Lattice(int size, int neighborNum, double interactionJ, double temperature)
@@ -91,7 +109,8 @@ Lattice::Lattice(int size, int neighborNum, double interactionJ, double temperat
         {
             for (int k=0; k<m_neighborNum; k++)
             {
-                configE += m_interactionJ * spinConfig[i*m_size+j] * spinConfig[neighbor[i*m_size+j][k]];
+                // configE += m_interactionJ * spinConfig[i*m_size+j] * spinConfig[neighbor[i*m_size+j][k]];
+                configE += m_interactionJ * Lattice::GetSiteInnerProduct(i*m_size+j,neighbor[i*m_size+j][k]);
             }
         }
     }
@@ -118,7 +137,9 @@ void Lattice::InitSpin()
     {
         for (int j=0; j<m_size; j++)
         {
-            spinConfig[i*m_size+j] = (intRand(engineTime) % 2) * 2 - 1;
+            Lattice::SetRandomSphericalDirection();
+            spinConfig[i*m_size+j][0] = temp_randomSphericalDirection[0];
+            spinConfig[i*m_size+j][1] = temp_randomSphericalDirection[1];
         }
     }
 }
@@ -130,7 +151,7 @@ void Lattice::ShowConfig()
     {
         for (int j=0; j<m_size; j++)
         {
-            cout << spinConfig[i*m_size+j] << " ";
+            cout << "(" << spinConfig[i*m_size+j][0] << "," << spinConfig[i*m_size+j][1] << ")|";
         }
         cout << endl;
     }
@@ -143,17 +164,35 @@ void Lattice::SweepFlip()
         for (int j=0; j<m_size; j++)
         {
             deltaE = 0;
-            int flipPos = intRand(engineTime) % (int) pow(m_size,2);
+            // int flipPos = intRand(engineTime) % (int) pow(m_size,2);
+            temp_flipPos = i * m_size + j;
+            temp_selectedSiteSpin[0] = spinConfig[temp_flipPos][0];
+            temp_selectedSiteSpin[1] = spinConfig[temp_flipPos][1];
+
+            Lattice::SetRandomSphericalDirection();
+
             for (int k=0; k<m_neighborNum; k++)
             {
-                deltaE += -2 * m_interactionJ * spinConfig[flipPos] * spinConfig[neighbor[flipPos][k]];
+                deltaE -= m_interactionJ * Lattice::GetSiteInnerProduct(temp_flipPos,neighbor[temp_flipPos][k]);
             }
 
+            spinConfig[temp_flipPos][0] = temp_randomSphericalDirection[0];
+            spinConfig[temp_flipPos][1] = temp_randomSphericalDirection[1];
+
+            for (int k=0; k<m_neighborNum; k++)
+            {
+                deltaE += m_interactionJ * Lattice::GetSiteInnerProduct(temp_flipPos,neighbor[temp_flipPos][k]);
+            }
 
             if (realRand(engineTime) < exp(-deltaE / m_temperature))
             {
-                spinConfig[flipPos] = -spinConfig[flipPos];
+                // spinConfig[flipPos] = -spinConfig[flipPos];
                 configE += deltaE;
+            }
+            else
+            {
+                spinConfig[temp_flipPos][0] = temp_selectedSiteSpin[0];
+                spinConfig[temp_flipPos][1] = temp_selectedSiteSpin[1];
             }
         }
     }
@@ -162,11 +201,11 @@ void Lattice::SweepFlip()
 
 int main()
 {
-    int latSize = 2;
+    int latSize = 1;
     int latNeighbor = 4;
     double interactionJ = -1.0;
 
-    double Tmin = 0.5;
+    double Tmin = 0.1;
     double Tmax = 5;
     double Tstep = 0.1;
     double temperature[(int)((Tmax-Tmin)/Tstep)];
@@ -178,6 +217,7 @@ int main()
     }
 
     int simulationCount = 10;
+    // int simulationCount = 1;
     int totalSweep = 1E5;
     // int sweepPerSample = totalSweep / 50;
     int sweepPerSample = 50;
@@ -199,6 +239,7 @@ int main()
     double sampledM;
     double sampledMsq;
     int totalSampleNum;
+    double energySequence[simulationCount][(int)(totalSweep*relaxationTime)];
 
     for (int simuindex=0; simuindex<simulationCount; simuindex++)
     {
@@ -216,7 +257,9 @@ int main()
 
             for (int i=0; i<totalSweep*relaxationTime; i++)
             {
-                lattice.SweepFlip();
+                configEnergy = lattice.GetEnergy();
+                energySequence[simuindex][i] = configEnergy;
+                lattice.SweepFlip();                
             }
 
             for (int i=0; i<totalSweep*(1-relaxationTime); i++)
@@ -224,25 +267,36 @@ int main()
                 lattice.SweepFlip();
                 if (i % sweepPerSample == 0)
                 {
-                    totalSampleNum++;
                     configEnergy = lattice.GetEnergy();
                     sampledEnergy += configEnergy;
                     sampledEsq += pow(configEnergy,2);
-                    configMagnetization = lattice.GetMagnetization();
-                    sampledM += configMagnetization;
-                    sampledMsq += pow(configMagnetization,2);
+                    totalSampleNum++;
+                    // configMagnetization = lattice.GetMagnetization();
+                    // sampledM += configMagnetization;
+                    // sampledMsq += pow(configMagnetization,2);
                 }
             }
 
             resultE[simuindex][tempindex] = sampledEnergy / totalSampleNum;
             resultEsq[simuindex][tempindex] = sampledEsq / totalSampleNum;
-            resultM[simuindex][tempindex] = sampledM / totalSampleNum;
-            resultMsq[simuindex][tempindex] = sampledMsq / totalSampleNum;
+            // resultM[simuindex][tempindex] = sampledM / totalSampleNum;
+            // resultMsq[simuindex][tempindex] = sampledMsq / totalSampleNum;
         }
     }
 
     clock_t calc_time = clock();
     cout << "Calculation finished, elapsed time: " << (double)(calc_time - start_time) / CLOCKS_PER_SEC << "s" << endl;
+
+    fout.open("validateEquilibrium.csv",ios::out);
+    for (int i=0; i<simulationCount; i++)
+    {
+        for (int j=0; j<totalSweep*relaxationTime; j++)
+        {
+            fout << energySequence[i][j] << ",";
+        }
+        fout << endl;
+    }
+    fout.close();
 
     fout.open("temperature.csv",ios::out);
     for (int i=0; i<tempLength; i++)
@@ -273,25 +327,25 @@ int main()
     }
     fout.close();
 
-    fout.open("resultM.csv",ios::out);
-    for (int i=0; i<simulationCount; i++)
-    {
-        for (int j=0; j<tempLength; j++)
-        {
-            fout << resultM[i][j] << ",";
-        }
-        fout << endl;
-    }
-    fout.close();
+    // fout.open("resultM.csv",ios::out);
+    // for (int i=0; i<simulationCount; i++)
+    // {
+    //     for (int j=0; j<tempLength; j++)
+    //     {
+    //         fout << resultM[i][j] << ",";
+    //     }
+    //     fout << endl;
+    // }
+    // fout.close();
 
-    fout.open("resultMsq.csv",ios::out);
-    for (int i=0; i<simulationCount; i++)
-    {
-        for (int j=0; j<tempLength; j++)
-        {
-            fout << resultMsq[i][j] << ",";
-        }
-        fout << endl;
-    }
+    // fout.open("resultMsq.csv",ios::out);
+    // for (int i=0; i<simulationCount; i++)
+    // {
+    //     for (int j=0; j<tempLength; j++)
+    //     {
+    //         fout << resultMsq[i][j] << ",";
+    //     }
+    //     fout << endl;
+    // }
     fout.close();
 }
--- diagram.py
+++ diagram.py
@@ -34,66 +34,111 @@ resultEsq = pd.read_csv("resultEsq.csv",header=None)
 resultEsq = resultEsq.dropna(axis=1, how='all')
 resultEsq = resultEsq.to_numpy()
 
-resultM = pd.read_csv("resultM.csv",header=None)
-resultM = resultM.dropna(axis=1, how='all')
-resultM = resultM.to_numpy()
+# resultM = pd.read_csv("resultM.csv",header=None)
+# resultM = resultM.dropna(axis=1, how='all')
+# resultM = resultM.to_numpy()
 
-resultMsq = pd.read_csv("resultMsq.csv",header=None)
-resultMsq = resultMsq.dropna(axis=1, how='all')
-resultMsq = resultMsq.to_numpy()
+# resultMsq = pd.read_csv("resultMsq.csv",header=None)
+# resultMsq = resultMsq.dropna(axis=1, how='all')
+# resultMsq = resultMsq.to_numpy()
+
+
 
 specificHeat = np.zeros((size[0],size[1]),dtype='float')
 for i in range(size[0]):
     for j in range(size[1]):
         specificHeat[i][j] = (resultEsq[i][j] - resultE[i][j] ** 2) / temperature[j] ** 2
         
-susceptibility = np.zeros((size[0],size[1]),dtype='float')
-for i in range(size[0]):
-    for j in range(size[1]):
-        susceptibility[i][j] = (resultMsq[i][j] - resultM[i][j] ** 2) / temperature[j]
+# susceptibility = np.zeros((size[0],size[1]),dtype='float')
+# for i in range(size[0]):
+#     for j in range(size[1]):
+#         susceptibility[i][j] = (resultMsq[i][j] - resultM[i][j] ** 2) / temperature[j]
 
-# resultE = np.mean(resultE/siteNum,axis=0)
-# deviationE = np.std(resultE/siteNum,axis=0)
+resultE = np.mean(resultE/siteNum,axis=0)
+deviationE = np.sqrt(np.var(resultE/siteNum,axis=0)/(size[0]-1))
 
 resultC = np.mean(specificHeat/siteNum,axis=0)
-deviationC = np.sqrt(np.var(specificHeat/siteNum,axis=0)/size[0])
-resultChi = np.mean(susceptibility/siteNum,axis=0)
-deviationChi = np.sqrt(np.var(susceptibility/siteNum,axis=0)/size[0])
+deviationC = np.sqrt(np.var(specificHeat/siteNum,axis=0)/(size[0]-1))
+# resultChi = np.mean(susceptibility/siteNum,axis=0)
+# deviationChi = np.sqrt(np.var(susceptibility/siteNum,axis=0)/(size[0]-1))
+
 # print(resultC)
 # print(deviationC)
 # print(resultChi)
 # print(deviationChi)
+# # %% 
+# baseline = np.zeros(size[1],dtype='float')
+
+# plt.figure(figsize=(16,8),dpi=200)
+# plt.subplot(2,2,1)
+# plt.errorbar(temperature, resultC, yerr=deviationC, marker='s',
+#          ms=2, mew=4, capsize=2, capthick=1)
+# plt.plot(temperature,theoryC,linewidth=3)
+# plt.title("C per site ~ T")
+plt.errorbar(temperature, resultE, yerr=deviationE, marker='s',
+        ms=2, mew=4, capsize=2, capthick=1)
+#plt.plot(temperature,theoryC,linewidth=3)
+plt.title("E per site ~ T")
+plt.savefig("Heisenberg2by2E-T")
+
+# plt.subplot(2,2,2)
+# plt.errorbar(temperature, resultChi, yerr=deviationChi, marker='s',
+#               ms=2, mew=4, capsize=2, capthick=1)
+# plt.plot(temperature,theoryChi,linewidth=3)
+# plt.title(r"$\chi$ per site ~ T")
+
+# plt.subplot(2,2,3)
+# plt.errorbar(temperature, resultC-theoryC, yerr=deviationC, marker='s',
+#          ms=2, mew=4, capsize=2, capthick=1)
+# plt.plot(temperature,baseline)
+# plt.title("C per site, MC-ED ~ T")
+
+# plt.subplot(2,2,4)
+# plt.errorbar(temperature, resultChi-theoryChi, yerr=deviationChi, marker='s',
+#          ms=2, mew=4, capsize=2, capthick=1)
+# plt.plot(temperature,baseline)
+# plt.ylim(-0.01,0.01)
+# plt.title(r"$\chi$ per site, MC-ED ~ T")
+
+# plt.savefig("isingErrorbarN=2.png")
 # %% 
 baseline = np.zeros(size[1],dtype='float')
 
-plt.figure(figsize=(16,8),dpi=200)
-plt.subplot(2,2,1)
+# plt.figure(figsize=(8,8),dpi=200)
+# plt.subplot(2,1,1)
 plt.errorbar(temperature, resultC, yerr=deviationC, marker='s',
-         ms=2, mew=4, capsize=2, capthick=1)
-plt.plot(temperature,theoryC,linewidth=3)
+          ms=2, mew=4, capsize=2, capthick=1)
+# plt.plot(temperature,theoryC,linewidth=3)
 plt.title("C per site ~ T")
-#plt.errorbar(temperature, resultE, yerr=deviationE, marker='s',
+plt.savefig("Heisenberg2by2C-T")
+
+# plt.subplot(2,1,2)
+# plt.errorbar(temperature, resultC-theoryC, yerr=deviationC, marker='s',
+#          ms=2, mew=4, capsize=2, capthick=1)
+# plt.plot(temperature,baseline)
+# plt.title("C per site, MC-ED ~ T")
+# %% thermalization
+
+valid = pd.read_csv("validateEquilibrium.csv",header=None)
+valid = valid.dropna(axis=1, how='all')
+sizeValid = valid.shape
+valid = valid.to_numpy()
+meanforValid = np.mean(valid/siteNum,axis=0)
+devforValid = np.sqrt(np.var(valid/siteNum,axis=0)/(size[0]-1))
+
+avgE = []
+
+for i in range(0,50):
+    avgE.append(np.mean(meanforValid[0:i+50]))
+
+for i in range(50,sizeValid[1]-50):
+    avgE.append(np.mean(meanforValid[i-50:i+50]))
+
+# valid = np.arange(sizeValid[1])
+# plt.errorbar(np.arange(100), meanforValid[0:100], yerr=devforValid[0:100], marker='s',
 #         ms=2, mew=4, capsize=2, capthick=1)
-##plt.plot(temperature,theoryC,linewidth=3)
-#plt.title("E per site ~ T")
-
-plt.subplot(2,2,2)
-plt.errorbar(temperature, resultChi, yerr=deviationChi, marker='s',
-              ms=2, mew=4, capsize=2, capthick=1)
-plt.plot(temperature,theoryChi,linewidth=3)
-plt.title(r"$\chi$ per site ~ T")
-
-plt.subplot(2,2,3)
-plt.errorbar(temperature, resultC-theoryC, yerr=deviationC, marker='s',
-         ms=2, mew=4, capsize=2, capthick=1)
-plt.plot(temperature,baseline)
-plt.title("C per site, MC-ED ~ T")
-
-plt.subplot(2,2,4)
-plt.errorbar(temperature, resultChi-theoryChi, yerr=deviationChi, marker='s',
-         ms=2, mew=4, capsize=2, capthick=1)
-plt.plot(temperature,baseline)
-plt.ylim(-0.01,0.01)
-plt.title(r"$\chi$ per site, MC-ED ~ T")
-
-plt.savefig("isingErrorbarN=2.png")
+# plt.title("E ~ MC Step")
+# plt.savefig("HeisenbergThermoE")
+plt.scatter(np.arange(2000),avgE[0:2000])
+plt.title("E' per site ~ MC Step")
+plt.savefig("HeisenbergThermoEprime")

ff1ed82d3dd8d22b2a8870586022f7b3be67c8d5
Zhuowei Zhang <zzw_question@126.com>
2022-10-23 01:58:28 +0800

    Added Triangular lattice and brute force Ising verification

--- IsingTriVerify.py
+++ IsingTriVerify.py
@@ -3,40 +3,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# theoretical value for 1*1 case
-theoryU = [-2.9, -2.80009, -2.70255, -2.61357, -2.53731, -2.47399, -2.42186, \
--2.37885, -2.34308, -2.31304, -2.28755, -2.26571, -2.24683, -2.23037, \
--2.2159, -2.2031, -2.1917, -2.18148, -2.17228, -2.16395, -2.15638, \
--2.14947, -2.14313, -2.13731, -2.13193, -2.12696, -2.12234, -2.11805, \
--2.11404, -2.1103, -2.10679, -2.10349, -2.1004, -2.09748, -2.09472, \
--2.09212, -2.08965, -2.08732, -2.0851, -2.08299, -2.08098, -2.07907, \
--2.07724, -2.0755, -2.07383, -2.07224, -2.07071, -2.06924, -2.06784]
-theoryC = [0.999999, 0.99546, 0.943294, 0.829258, 0.695913, 0.573753, 0.472287, \
-0.39111, 0.326861, 0.275938, 0.235297, 0.202559, 0.175921, 0.154025, \
-0.135854, 0.120634, 0.107776, 0.0968282, 0.087437, 0.0793264, \
-0.0722774, 0.0661153, 0.0606993, 0.055915, 0.0516691, 0.0478844, \
-0.0444971, 0.041454, 0.0387103, 0.0362283, 0.033976, 0.031926, \
-0.0300551, 0.028343, 0.0267723, 0.0253281, 0.023997, 0.0227678, \
-0.0216302, 0.0205755, 0.0195958, 0.0186841, 0.0178344, 0.0170412, \
-0.0162996, 0.0156052, 0.0149541, 0.0143429, 0.0137682]
-theoryM = [0.9, 0.800091, 0.702549, 0.613567, 0.537315, 0.473987, 0.421864, \
-0.378851, 0.343078, 0.313035, 0.287548, 0.265713, 0.246834, 0.230371, \
-0.215905, 0.203102, 0.191699, 0.181483, 0.172282, 0.163953, 0.156381, \
-0.149468, 0.143133, 0.137308, 0.131932, 0.126958, 0.122342, 0.118047, \
-0.114042, 0.110297, 0.106788, 0.103495, 0.100397, 0.0974784, \
-0.0947238, 0.0921198, 0.0896544, 0.087317, 0.0850978, 0.0829882, \
-0.0809802, 0.0790667, 0.0772413, 0.075498, 0.0738314, 0.0722365, \
-0.0707089, 0.0692443, 0.0678391]
-theoryChi = [0.0999999, 0.199092, 0.282988, 0.331703, 0.347956, 0.344252, \
-0.330601, 0.312888, 0.294175, 0.275938, 0.258827, 0.243071, 0.228697, \
-0.215636, 0.203781, 0.193014, 0.18322, 0.174291, 0.16613, 0.158653, \
-0.151783, 0.145454, 0.139608, 0.134196, 0.129173, 0.124499, 0.120142, \
-0.116071, 0.11226, 0.108685, 0.105326, 0.102163, 0.0991817, 0.096366, \
-0.0937031, 0.091181, 0.0887891, 0.0865176, 0.0843578, 0.0823019, \
-0.0803426, 0.0784733, 0.076688, 0.0749813, 0.0733482, 0.071784, \
-0.0702845, 0.0688458, 0.0674643]
-    
-siteNum = 1
+siteNum = 3*3
+
+# theoretical value
+theory = pd.read_csv("theoretical.csv",header=None)
+theory = theory.dropna(axis=1, how='all')
+theoryU = theory.to_numpy()[0] / siteNum
+theoryC = theory.to_numpy()[1] / siteNum
+theoryM = theory.to_numpy()[2] / siteNum
+theoryChi = theory.to_numpy()[3] / siteNum
 
 temperature = pd.read_csv("temperature.csv",header=None)
 temperature = temperature.dropna(axis=1, how='all')
@@ -89,7 +64,7 @@ ax1.errorbar(temperature, resultU, yerr=deviationU, marker='o',
 ax1.plot(temperature,theoryU,linewidth=3, alpha=0.2)
 ax2 = ax1.twinx()
 ax2.errorbar(temperature, resultU-theoryU, yerr=deviationU, marker='o',
-         ms=1, mew=4, capsize=4, capthick=1, linestyle='none')
+          ms=1, mew=4, capsize=4, capthick=1, linestyle='none')
 ax2.plot(temperature, baseline, linestyle='dashed')
 ax1.set_title("E per site ~ T")
 
@@ -101,6 +76,7 @@ specificHeatax1.plot(temperature,theoryC,linewidth=3, alpha=0.2)
 specificHeatax2 = specificHeatax1.twinx()
 specificHeatax2.errorbar(temperature, resultC-theoryC, yerr=deviationC, marker='o',
           ms=1, mew=4, capsize=4, capthick=1, linestyle='none')
+specificHeatax2.set_ylim(-0.01,0.01)
 specificHeatax2.plot(temperature, baseline, linestyle='dashed')
 specificHeatax1.set_title("C per site ~ T")
 
@@ -111,6 +87,7 @@ magax1.plot(temperature,theoryM,linewidth=3, alpha=0.2)
 magax2 = magax1.twinx()
 magax2.errorbar(temperature, resultaveM-theoryM, yerr=deviationM, marker='o',
           ms=1, mew=4, capsize=4, capthick=1, linestyle='none')
+magax2.set_ylim(-0.02,0.02)
 magax2.plot(temperature, baseline, linestyle='dashed')
 magax1.set_title("M per site ~ T")
 
@@ -121,33 +98,8 @@ susceptibilityax1.plot(temperature,theoryChi,linewidth=3, alpha=0.2)
 susceptibilityax2 = susceptibilityax1.twinx()
 susceptibilityax2.errorbar(temperature, resultChi-theoryChi, yerr=deviationChi, marker='o',
           ms=1, mew=4, capsize=4, capthick=1, linestyle='none')
+susceptibilityax2.set_ylim(-0.02,0.02)
 susceptibilityax2.plot(temperature, baseline, linestyle='dashed')
 susceptibilityax1.set_title(r"$\chi$ per site ~ T")
 
-plt.savefig("Heisenberg1by1ExternalField")
-
-# %% thermalization
-
-valid = pd.read_csv("validateEquilibrium.csv",header=None)
-valid = valid.dropna(axis=1, how='all')
-sizeValid = valid.shape
-valid = valid.to_numpy()
-meanforValid = np.mean(valid/siteNum,axis=0)
-devforValid = np.sqrt(np.var(valid/siteNum,axis=0)/(size[0]-1))
-
-avgE = []
-
-for i in range(0,50):
-    avgE.append(np.mean(meanforValid[0:i+50]))
-
-for i in range(50,sizeValid[1]-50):
-    avgE.append(np.mean(meanforValid[i-50:i+50]))
-
-# valid = np.arange(sizeValid[1])
-# plt.errorbar(np.arange(100), meanforValid[0:100], yerr=devforValid[0:100], marker='s',
-#         ms=2, mew=4, capsize=2, capthick=1)
-# plt.title("E ~ MC Step")
-# plt.savefig("HeisenbergThermoE")
-plt.plot(np.arange(9000),avgE[0:9000])
-plt.title("E' per site ~ MC Step")
-plt.savefig("HeisenbergThermoEprime")
+plt.savefig("IsingVerify.png")
--- TriMottMC.cpp
+++ TriMottMC.cpp
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <random>
 #include <cmath>
+#define TEMPERATURESEQUENCEMAX 1000
 using namespace std;
 
 std::uniform_real_distribution<double> realRand(0,1);
@@ -16,9 +17,12 @@ class Lattice
     static const int NEIGHBOR_MAX = 6;
 
     public:
-        Lattice(int row, int column, int neighborNum, double interactionJ, double temperature, double externalMagField);
+        Lattice(int row, int column, string latType, double interactionJ, double temperature, double externalMagField, string interactionType);
 
-        void SetNeighborIndex();
+        int m_size1;
+        int m_size2;
+
+        void SetNeighborIndex(string latType);
         void InitSpin();
         void ShowConfig();
         void SweepFlip();
@@ -31,9 +35,19 @@ class Lattice
         double GetMagnetization()
         {
             configM = 0;
-            for (int i=0; i<m_size1*m_size2; i++)
+            if (m_interactionType == "Heisenberg")
             {
-                configM += GetSiteZComponent(i);
+                for (int i=0; i<m_size1*m_size2; i++)
+                {
+                    configM += GetSiteZComponent(i);
+                }
+            }
+            else if (m_interactionType == "Ising")
+            {
+                for (int i=0; i<m_size1*m_size2; i++)
+                {
+                    configM += spinConfig[i][0];
+                }
             }
             return configM;
         }
@@ -41,23 +55,24 @@ class Lattice
         void DebugOutput()
         {
             ShowConfig();
-            cout << GetSiteInnerProduct(0,1) << endl;
+            // cout << GetSiteInnerProduct(0,1) << endl;
         }
 
-        ~Lattice()
-        {
-            delete temp_stackedIndex;
-            delete temp_randomSphericalDirection;
-            delete temp_selectedSiteSpin;
-        }
+        void GetBinaryConfigIsingEM(int binaryCode, double result[2]);
+
+        // ~Lattice()
+        // {
+        //     delete temp_stackedIndex;
+        //     delete temp_randomSphericalDirection;
+        //     delete temp_selectedSiteSpin;
+        // }
 
     private:
-        int m_size1;
-        int m_size2;
         int m_neighborNum;
         double m_interactionJ;
         double m_temperature;
         double m_externalMagField;
+        string m_interactionType;
 
         double configE;
         double deltaE;
@@ -65,10 +80,15 @@ class Lattice
 
         double spinConfig[LATTICESIZE_MAX * LATTICESIZE_MAX][2];
         int neighbor[LATTICESIZE_MAX * LATTICESIZE_MAX][NEIGHBOR_MAX];
-        int *temp_stackedIndex = new int[2];
-        double *temp_randomSphericalDirection = new double[2];
+        // int *temp_stackedIndex = new int[2];
+        // double *temp_randomSphericalDirection = new double[2];
+        // int temp_flipPos;
+        // double *temp_selectedSiteSpin = new double[2];
+        int temp_stackedIndex[2];
+        double temp_randomSphericalDirection[2];
         int temp_flipPos;
-        double *temp_selectedSiteSpin = new double[2];
+        double temp_selectedSiteSpin[2];
+
 
         int GetFlattenedCoordinate(int stackedIndex1, int stackedIndex2)
         {
@@ -104,56 +124,124 @@ class Lattice
         }
 };
 
-Lattice::Lattice(int row, int column, int neighborNum, double interactionJ, double temperature, double externalMagField=0)
-    : m_size1(column), m_size2(row), m_neighborNum(neighborNum), m_interactionJ(interactionJ), m_temperature(temperature), m_externalMagField(externalMagField)
+Lattice::Lattice(int row, int column, string latType, double interactionJ, double temperature, double externalMagField=0, string interactionType="Ising")
+    : m_size1(column), m_size2(row), m_interactionJ(interactionJ), m_temperature(temperature), m_externalMagField(externalMagField), m_interactionType(interactionType)
 {
-    SetNeighborIndex();
+    SetNeighborIndex(latType);
     InitSpin();
-    configE = 0;
-    for (int i=0; i<m_size1; i++)
-    {
-        for (int j=0; j<m_size2; j++)
-        {
-            for (int k=0; k<m_neighborNum; k++)
-            {
-                // configE += m_interactionJ * spinConfig[i*m_size+j] * spinConfig[neighbor[i*m_size+j][k]];
-                configE += m_interactionJ * Lattice::GetSiteInnerProduct(i*m_size1+j,neighbor[i*m_size1+j][k]) / 2;
-            }
-            configE -= m_externalMagField * Lattice::GetSiteZComponent(i*m_size1+j);
-        }
-    }
 }
 
-void Lattice::SetNeighborIndex()
+void Lattice::SetNeighborIndex(string latType)
 {
-    for (int i=0; i<m_size1; i++)
+    if (latType == "Square")
     {
-        for (int j=0; j<m_size2; j++)
+        m_neighborNum = 4;
+        for (int i=0; i<m_size1; i++)
         {
-            neighbor[i*m_size1+j][0] = Lattice::GetFlattenedCoordinate(i,j+1);
-            neighbor[i*m_size1+j][1] = Lattice::GetFlattenedCoordinate(i-1,j);
-            neighbor[i*m_size1+j][2] = Lattice::GetFlattenedCoordinate(i,j-1);
-            neighbor[i*m_size1+j][3] = Lattice::GetFlattenedCoordinate(i+1,j);
+            for (int j=0; j<m_size2; j++)
+            {
+                neighbor[i*m_size1+j][0] = Lattice::GetFlattenedCoordinate(i,j+1);
+                neighbor[i*m_size1+j][1] = Lattice::GetFlattenedCoordinate(i-1,j);
+                neighbor[i*m_size1+j][2] = Lattice::GetFlattenedCoordinate(i,j-1);
+                neighbor[i*m_size1+j][3] = Lattice::GetFlattenedCoordinate(i+1,j);
+            }
         }
     }
+    else if (latType == "Hexagon")
+    {
+        m_neighborNum = 3;
+        for (int i=0; i<m_size1; i++)
+        {
+            for (int j=0; j<m_size2; j++)
+            {
+                neighbor[i*m_size1+j][0] = Lattice::GetFlattenedCoordinate(i,j+1);
+                neighbor[i*m_size1+j][1] = Lattice::GetFlattenedCoordinate(i,j-1);
+                if (i % 2 == 0)
+                {
+                    neighbor[i*m_size1+j][2] = Lattice::GetFlattenedCoordinate(i+1,j+1);
+                }
+                else
+                {
+                    neighbor[i*m_size1+j][2] = Lattice::GetFlattenedCoordinate(i-1,j-1);
+                }
+            }
+        }
+    }
+    else if (latType == "Triangle")
+    {
+        m_neighborNum = 6;
+        for (int i=0; i<m_size1; i++)
+        {
+            for (int j=0; j<m_size2; j++)
+            {
+                neighbor[i*m_size1+j][0] = Lattice::GetFlattenedCoordinate(i,j+1);
+                neighbor[i*m_size1+j][1] = Lattice::GetFlattenedCoordinate(i-1,j);
+                neighbor[i*m_size1+j][2] = Lattice::GetFlattenedCoordinate(i-1,j-1);
+                neighbor[i*m_size1+j][3] = Lattice::GetFlattenedCoordinate(i,j-1);
+                neighbor[i*m_size1+j][4] = Lattice::GetFlattenedCoordinate(i+1,j);
+                neighbor[i*m_size1+j][5] = Lattice::GetFlattenedCoordinate(i+1,j+1);
+            }
+        }
+    }
+    // Throw exception
 }
 
 void Lattice::InitSpin()
 {
-    for (int i=0; i<m_size1; i++)
+    configE = 0;
+    if (m_interactionType == "Heisenberg")
     {
-        for (int j=0; j<m_size2; j++)
+        for (int i=0; i<m_size1; i++)
         {
-            Lattice::SetRandomSphericalDirection();
-            spinConfig[i*m_size1+j][0] = temp_randomSphericalDirection[0];
-            spinConfig[i*m_size1+j][1] = temp_randomSphericalDirection[1];
+            for (int j=0; j<m_size2; j++)
+            {
+                Lattice::SetRandomSphericalDirection();
+                spinConfig[i*m_size1+j][0] = temp_randomSphericalDirection[0];
+                spinConfig[i*m_size1+j][1] = temp_randomSphericalDirection[1];
+            }
+        }
+
+        for (int i=0; i<m_size1; i++)
+        {
+            for (int j=0; j<m_size2; j++)
+            {
+                for (int k=0; k<m_neighborNum; k++)
+                {
+                    configE += m_interactionJ * Lattice::GetSiteInnerProduct(i*m_size1+j,neighbor[i*m_size1+j][k]) / 2;
+                }
+                configE -= m_externalMagField * Lattice::GetSiteZComponent(i*m_size1+j);
+            }
         }
     }
+
+    else if (m_interactionType == "Ising")
+    {
+        for (int i=0; i<m_size1; i++)
+        {
+            for (int j=0; j<m_size2; j++)
+            {
+                spinConfig[i*m_size1+j][0] = (intRand(engineTime) % 2) * 2 - 1;
+            }
+        }
+
+        for (int i=0; i<m_size1; i++)
+        {
+            for (int j=0; j<m_size2; j++)
+            {
+                for (int k=0; k<m_neighborNum; k++)
+                {
+                    configE += m_interactionJ * spinConfig[i*m_size1+j][0] * spinConfig[neighbor[i*m_size1+j][k]][0] / 2;
+                }
+                configE -= m_externalMagField * spinConfig[i*m_size1+j][0];
+            }
+        }
+    }
+    // Throw exception
 }
 
 void Lattice::ShowConfig()
 {
-    cout << "Config:" << endl;
+    cout << m_interactionType << ", Config:" << endl;
     for (int i=0; i<m_size1; i++)
     {
         for (int j=0; j<m_size2; j++)
@@ -162,6 +250,7 @@ void Lattice::ShowConfig()
         }
         cout << endl;
     }
+    cout << GetEnergy() << endl;
 }
 
 void Lattice::SweepFlip()
@@ -171,57 +260,139 @@ void Lattice::SweepFlip()
         for (int j=0; j<m_size2; j++)
         {
             deltaE = 0;
-            // int flipPos = intRand(engineTime) % (int) pow(m_size,2);
-            temp_flipPos = i * m_size1 + j;
-            temp_selectedSiteSpin[0] = spinConfig[temp_flipPos][0];
-            temp_selectedSiteSpin[1] = spinConfig[temp_flipPos][1];
 
-            Lattice::SetRandomSphericalDirection();
-
-            for (int k=0; k<m_neighborNum; k++)
+            if (m_interactionType == "Heisenberg")
             {
-                deltaE -= m_interactionJ * Lattice::GetSiteInnerProduct(temp_flipPos,neighbor[temp_flipPos][k]);
+                temp_flipPos = i * m_size1 + j;
+                temp_selectedSiteSpin[0] = spinConfig[temp_flipPos][0];
+                temp_selectedSiteSpin[1] = spinConfig[temp_flipPos][1];
+
+                Lattice::SetRandomSphericalDirection();
+
+                for (int k=0; k<m_neighborNum; k++)
+                {
+                    deltaE -= m_interactionJ * Lattice::GetSiteInnerProduct(temp_flipPos,neighbor[temp_flipPos][k]);
+                }
+                deltaE += m_externalMagField * Lattice::GetSiteZComponent(temp_flipPos);
+
+                spinConfig[temp_flipPos][0] = temp_randomSphericalDirection[0];
+                spinConfig[temp_flipPos][1] = temp_randomSphericalDirection[1];
+
+                for (int k=0; k<m_neighborNum; k++)
+                {
+                    deltaE += m_interactionJ * Lattice::GetSiteInnerProduct(temp_flipPos,neighbor[temp_flipPos][k]);
+                }
+                deltaE -= m_externalMagField * Lattice::GetSiteZComponent(temp_flipPos);
+
+                if (realRand(engineTime) < exp(-deltaE / m_temperature))
+                {
+                    // spinConfig[flipPos] = -spinConfig[flipPos];
+                    configE += deltaE;
+                }
+                else
+                {
+                    spinConfig[temp_flipPos][0] = temp_selectedSiteSpin[0];
+                    spinConfig[temp_flipPos][1] = temp_selectedSiteSpin[1];
+                }
             }
-            deltaE += m_externalMagField * Lattice::GetSiteZComponent(temp_flipPos);
 
-            spinConfig[temp_flipPos][0] = temp_randomSphericalDirection[0];
-            spinConfig[temp_flipPos][1] = temp_randomSphericalDirection[1];
+            else if (m_interactionType == "Ising")
+            {
+                temp_flipPos = intRand(engineTime) % (m_size1*m_size2);
+                for (int k=0; k<m_neighborNum; k++)
+                {
+                    deltaE += -2 * m_interactionJ * spinConfig[temp_flipPos][0] * spinConfig[neighbor[temp_flipPos][k]][0];
+                }
+                deltaE -= -2 * m_externalMagField * spinConfig[temp_flipPos][0];
 
-            for (int k=0; k<m_neighborNum; k++)
-            {
-                deltaE += m_interactionJ * Lattice::GetSiteInnerProduct(temp_flipPos,neighbor[temp_flipPos][k]);
-            }
-            deltaE -= m_externalMagField * Lattice::GetSiteZComponent(temp_flipPos);
-
-            if (realRand(engineTime) < exp(-deltaE / m_temperature))
-            {
-                // spinConfig[flipPos] = -spinConfig[flipPos];
-                configE += deltaE;
-            }
-            else
-            {
-                spinConfig[temp_flipPos][0] = temp_selectedSiteSpin[0];
-                spinConfig[temp_flipPos][1] = temp_selectedSiteSpin[1];
+                if (realRand(engineTime) < exp(-deltaE / m_temperature))
+                {
+                    spinConfig[temp_flipPos][0] = -spinConfig[temp_flipPos][0];
+                    configE += deltaE;
+                }
             }
         }
     }
 }
 
+void Lattice::GetBinaryConfigIsingEM(int binaryCode, double result[2])
+{
+    double binary_configE=0;
+
+    for (int i=0; i<m_size1*m_size2; i++)
+    {
+        if (binaryCode>>i&1)
+        {
+            spinConfig[i][0] = 1;
+        }
+        else
+        {
+            spinConfig[i][0] = -1;
+        }
+    }
+
+    for (int i=0; i<m_size1; i++)
+    {
+        for (int j=0; j<m_size2; j++)
+        {
+            for (int k=0; k<m_neighborNum; k++)
+            {
+                binary_configE += m_interactionJ * spinConfig[i*m_size1+j][0] * spinConfig[neighbor[i*m_size1+j][k]][0] / 2;
+            }
+            binary_configE -= m_externalMagField * spinConfig[i*m_size1+j][0];
+        }
+    }
+
+    result[0] = binary_configE;
+    result[1] = GetMagnetization();
+}
+
+void CalculateTheoreticalParams(Lattice lattice, int tempNum, double temperatureSequence[TEMPERATURESEQUENCEMAX], double resultParam[4][TEMPERATURESEQUENCEMAX])
+{
+    double Z[tempNum] = {0.0};
+    double HZ[tempNum] = {0.0};
+    double HsqZ[tempNum] = {0.0};
+    double MZ[tempNum] = {0.0};
+    double MsqZ[tempNum] = {0.0};
+
+    double Energy_Magnetization[2];
+
+    for (int binConfig=0; binConfig<(1<<lattice.m_size1*lattice.m_size2); binConfig++)
+    {
+        lattice.GetBinaryConfigIsingEM(binConfig,Energy_Magnetization);
+        for (int tempindex=0; tempindex<tempNum; tempindex++)
+        {
+            Z[tempindex] += exp(-Energy_Magnetization[0]/temperatureSequence[tempindex]);
+            HZ[tempindex] += Energy_Magnetization[0] * exp(-Energy_Magnetization[0]/temperatureSequence[tempindex]);
+            HsqZ[tempindex] += pow(Energy_Magnetization[0],2) * exp(-Energy_Magnetization[0]/temperatureSequence[tempindex]);
+            MZ[tempindex] += Energy_Magnetization[1] * exp(-Energy_Magnetization[0]/temperatureSequence[tempindex]);
+            MsqZ[tempindex] += pow(Energy_Magnetization[1],2) * exp(-Energy_Magnetization[0]/temperatureSequence[tempindex]);
+        }
+    }
+    for (int tempindex=0; tempindex<tempNum; tempindex++)
+    {
+        resultParam[0][tempindex] = HZ[tempindex] / Z[tempindex];
+        resultParam[1][tempindex] = 1 / pow(temperatureSequence[tempindex],2) * (HsqZ[tempindex] * Z[tempindex] - pow(HZ[tempindex],2)) / pow(Z[tempindex],2);
+        resultParam[2][tempindex] = MZ[tempindex] / Z[tempindex];
+        resultParam[3][tempindex] = 1 / temperatureSequence[tempindex] * (MsqZ[tempindex] * Z[tempindex] - pow(MZ[tempindex],2)) / pow(Z[tempindex],2);
+    }
+}
 
 int main()
 {
-    int latSizeRow = 1;
-    int latSizeColumn = 2;
-    int latNeighbor = 4;
+    int latSizeXMax = 3;
+    int latSizeYMax = 3;
+    string latType = "Triangle";
     double interactionJ = -1.0;
-    double externalZMageticField = 0;
+    double externalZMageticField = 1;
+    string interactionType = "Ising";
 
     double Tmin = 0.1;
     double Tmax = 5;
     double Tstep = 0.1;
-    double temperature[(int)((Tmax-Tmin)/Tstep)];
+    double temperature[TEMPERATURESEQUENCEMAX];
 
-    int tempLength = sizeof(temperature) / sizeof(temperature[0]);
+    int tempLength = (int)((Tmax-Tmin)/Tstep);
     for (int i=0; i<tempLength; i++)
     {
         temperature[i] = Tmin + Tstep * i;
@@ -250,7 +421,18 @@ int main()
     double sampledM;
     double sampledMsq;
     int totalSampleNum;
+    double theoreticalResult[4][TEMPERATURESEQUENCEMAX];
     // double energySequence[simulationCount][(int)(totalSweep*relaxationTime)];
+
+    // Lattice lattice(latSizeXMax,latSizeYMax,latType,interactionJ,1,externalZMageticField,interactionType);
+    // double ans[2];
+    // lattice.GetBinaryConfigIsingEM(15,ans);
+    // cout << ans[0] << " " << ans[1] << endl;
+    if (interactionType == "Ising")
+    {
+        Lattice lattice(latSizeXMax,latSizeYMax,latType,interactionJ,1,externalZMageticField,interactionType);
+        CalculateTheoreticalParams(lattice, tempLength, temperature, theoreticalResult);
+    }
 
     for (int simuindex=0; simuindex<simulationCount; simuindex++)
     {
@@ -264,7 +446,7 @@ int main()
             sampledMsq = 0;
             totalSampleNum = 0;
 
-            Lattice lattice(latSizeRow,latSizeColumn,latNeighbor,interactionJ,temperature[tempindex],externalZMageticField);
+            Lattice lattice(latSizeXMax,latSizeYMax,latType,interactionJ,temperature[tempindex],externalZMageticField,interactionType);
 
             for (int i=0; i<totalSweep*relaxationTime; i++)
             {
@@ -272,6 +454,9 @@ int main()
                 // energySequence[simuindex][i] = configEnergy;
                 lattice.SweepFlip();
             }
+            // lattice.DebugOutput();
+            // lattice.SweepFlip();
+            // lattice.DebugOutput();
 
             for (int i=0; i<totalSweep*(1-relaxationTime); i++)
             {
@@ -294,6 +479,7 @@ int main()
             resultMsq[simuindex][tempindex] = sampledMsq / totalSampleNum;
         }
     }
+
 
     clock_t calc_time = clock();
     cout << "Calculation finished, elapsed time: " << (double)(calc_time - start_time) / CLOCKS_PER_SEC << "s" << endl;
@@ -355,6 +541,17 @@ int main()
         for (int j=0; j<tempLength; j++)
         {
             fout << resultMsq[i][j] << ",";
+        }
+        fout << endl;
+    }
+    fout.close();
+
+    fout.open("theoretical.csv",ios::out);
+    for (int i=0; i<4; i++)
+    {
+        for (int j=0; j<tempLength; j++)
+        {
+            fout << theoreticalResult[i][j] << ",";
         }
         fout << endl;
     }
